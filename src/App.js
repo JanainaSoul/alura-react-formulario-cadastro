a2c07f54e787358d4734f93ef33654a0a4cf8abf
Janaina <janainasoul@icloud.com>
2023-05-15 15:00:33 -0300

    Tipografia e Switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import { Component } from 'react';
 import FormularioCadastro from './Components/FormularioCadastro/FormularioCadastro';
-import {Container} from '@material-ui/core'
+import {Container, Typography} from '@material-ui/core';
+//import '@fontsource-roboto';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 class App extends Component {
 
   render () {
     return (
       <Container component="article" maxWidth="sm" > 
-          <h1> Formulario de Cadastro</h1>
+          <Typography variant="h3" component="h1" align="center" > Formulario de Cadastro</Typography>
       <FormularioCadastro/>
       </Container>
     
--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,20 +1,44 @@
 import {render} from '@testing-library/react';
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField  from '@material-ui/core/TextField';
+//import Button from '@material-ui/core/Button';
+import {TextField, Switch, Button, FormControlLabel} from '@material-ui/core';
+
 
 function FormularioCadastro(){
     return (
         <form> 
-            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
-            <TextField id="Sobrenome" label="Sobrenome" variant="outlined"  margin="normal" fullWidth/>
-            <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth/>
-            
-            <label> Promoções</label>
-            <input type="checkbox"/>
-            <label> Novidades</label>
-            <input type="checkbox"/>
+            <TextField
+                id="nome" 
+                label="Nome" 
+                variant="outlined"
+                margin="normal" 
+                fullWidth 
+            />
+            <TextField
+                id="Sobrenome" 
+                label="Sobrenome" 
+                variant="outlined"  
+                margin="normal" 
+                fullWidth
+            />
+            <TextField 
+                id="cpf" 
+                label="CPF" 
+                variant="outlined" 
+                margin="normal" 
+                fullWidth
+            />
 
+            
+            <FormControlLabel 
+                label="Promoções" 
+                control = {<Switch name="promocoes" defaultChecked color="primary"/> }
+            />
+            <FormControlLabel 
+                label="Novidades" 
+                control = {<Switch name="novidade" defaultChecked color="primary"/> }
+            />
+                        
             <Button type="submit" variant="contained" color="primary"> Cadastrar</Button>
             
         </form>
